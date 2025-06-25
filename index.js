0b8371245f830090fa1d3b18bb576f4bec552880
Fake guy <temp@MacBookPro.fritz.box>
2025-06-25 15:57:45 +0200

    made background more efficient, added easter egg

--- index.js
+++ index.js
@@ -1,26 +1,96 @@
-const honeycombEl = document.getElementById("honeycomb")
-const combHeight = 40
-const combWidth = 48
-const hexCountY = Math.ceil(honeycombEl.clientHeight / combHeight) + 1
-const hexCountX = Math.ceil(honeycombEl.clientWidth / combWidth) + 1
+// Getting data
+let eggData;
+fetch("./assets/files/egg.json")
+    .then(response => response.json())
+    .then(data => eggData = data);
 
-function backgroundHoneycombs() {
-    for (let i = 0; i < hexCountY; i++) {
-        for (let j = 0; j < hexCountX; j++) {
-            const hexagon = document.createElement("div")
-            hexagon.style = `
-            background: url(assets/images/Hexagon.png) no-repeat;
-            background-size: contain;
-            width: 100px;
-            height: 70px;
-            position: absolute;
-            top: ${i * 40}px;
-            left: ${j * 48 + ((i * 24) % 48)}px; 
-            `
-            honeycombEl.appendChild(hexagon)
+// Variables
+let dashArray = [];
+let eggActive = false;
+let lettersFound = 0;
+const eggElements = 6;
+const eggStartDelay = 5000;
+const headshotEl = document.getElementById("headshot");
+const eggContainerEl = document.getElementById("egg-container");
+let eggLettersArray = document.getElementsByClassName("for-egg");
+
+// Easter Egg Implementation
+function flipDash() {
+    dashArray[lettersFound].innerText = " ? ";
+}
+
+function eggIsDone() {
+    if (lettersFound === eggElements) {
+        for (const element of eggLettersArray) {
+            element.classList.remove("text-pulse");
+            element.classList.add("glow");
         }
+        eeLink = document.createElement("a");
+        eeLink.href = "https://www.youtube.com/watch?v=X0IPjZDoB9U";
+        eeLink.target = "_blank";
+        eeButton = document.createElement("button");
+        eeButton.type = "button";
+        eeButton.id = "ee-button";
+        eeButton.classList.add("fade-in");
+        eeButton.innerText = "Claim Reward";
+        eeLink.appendChild(eeButton)
+        eggContainerEl.appendChild(eeLink);
+        eeButton.addEventListener("click", () => {
+            for (const element of eggLettersArray) {
+                element.classList.remove("glow");
+            }
+            for (let i = 0; i < dashArray.length; i++) {
+                dashArray[i].innerText = ` ${eggData.finalText[i]} `
+            }
+        })
     }
 }
 
-backgroundHoneycombs()
-window.addEventListener("resize", backgroundHoneycombs)
+setTimeout(() => {
+    headshotEl.style.cursor = "pointer";
+    headshotEl.classList.add("pulse");
+}, eggStartDelay)
+
+headshotEl.addEventListener("click", () => {
+    if (!eggActive) {
+        eggActive = true;
+        eggContainerEl.style.border = "#ffe4c4 1px solid";
+        headshotEl.classList.remove("pulse");
+        for (const element of eggLettersArray) {
+            element.style.cursor = "pointer";
+            element.addEventListener("click", () => {
+                if (!element.classList.contains("clicked")) {
+                    element.classList.add("clicked", "text-pulse");
+                    flipDash();
+                    lettersFound++;
+                    eggIsDone();
+                } else {
+                    alert("Letter already found!")
+                }
+            });
+        }
+
+        const eggTitle = document.createElement("h3");
+        eggTitle.id = "ee-title";
+        eggTitle.innerText = eggData.title;
+        eggContainerEl.appendChild(eggTitle);
+
+        const eggBody = document.createElement("p");
+        eggBody.id = "ee-body";
+        eggBody.innerText = eggData.instructions;
+        eggContainerEl.appendChild(eggBody);
+
+        const eggDashes = document.createElement("div");
+        eggDashes.id = "ee-dash-container";
+        for (let i = 0; i < eggElements; i++) {
+            let dash = document.createElement("span");
+            dash.id = "dash";
+            dash.innerText = " _ ";
+            dashArray.push(dash);
+            eggDashes.appendChild(dash);
+        }
+        eggContainerEl.appendChild(eggDashes);
+
+        eggContainerEl.classList.add("fade-in");
+    }
+})
